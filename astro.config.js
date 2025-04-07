adaa8773614dfba093e0a994d7163e5e1c5570f0
Bryan Hoang <bryan@bryanhoang.dev>
2025-04-07 16:07:34 -0400

    chore(deps): update starlight to v0.33.0

--- astro.config.js
+++ astro.config.js
@@ -8,9 +8,13 @@ export default defineConfig({
 			title: "Bryan Hoang's D&D Notes",
 			description:
 				"Notes I write whenever I have a computer in front of me during sessions :D",
-			social: {
-				github: "https://github.com/bryan-hoang/dnd-notes",
-			},
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/bryan-hoang/dnd-notes",
+				},
+			],
 			favicon: "/img/favicon.ico",
 			logo: {
 				src: "./src/assets/logo.png",
